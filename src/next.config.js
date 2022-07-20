0fd8859c499fe1e16f978092b24606a41dc685a3
kitasan04 <yuji.a.20010416@gmail.com>
2022-07-20 20:03:14 +0900

    a

--- src/next.config.js
+++ src/next.config.js
@@ -2,12 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint:{
+	  ignoreDuringBuilds: true,
+  },
 }
 
-module.exports = {
-	eslint: {
-	// Warning: This allows production builds to successfully complete even if
-       // your project has ESLint errors.
-	ignoreDuringBuilds: true,
-	},
-}
+module.exports =nextConfig
